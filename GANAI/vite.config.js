6970f084f69dcf7c2c2379c7480af44ef51a5174
TienDucTran <tienduc20052000@gmail.com>
2025-12-19 04:20:54 +0700

    fix vercel 404 routing

--- GANAI/vite.config.js
+++ GANAI/vite.config.js
@@ -3,16 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
-    server: {
-        host: true,
-        port: 3000, // use a different port like 3000
-        strictPort: true, // force it to use the chosen port
-        allowedHosts: [
-            'localhost',
-            '127.0.0.1',
-            'd61e49f1342f.ngrok-free.app', // <-- your ngrok domain
-        ],
-        cors: true,
-    },
+  plugins: [react()],
+  base: '/',
 });
